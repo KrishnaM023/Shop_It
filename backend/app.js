920f884fe8301f5dec6abf5a87678a04ac30dd6b
KrishnaM023 <krishna050120>
2024-07-08 10:48:55 +0530

    Handling Unhandled Promise rejection

--- backend/app.js
+++ backend/app.js
@@ -21,8 +21,18 @@ app.use("/api/v1", prodductRoutes);
 // Using error middleware
 app.use(errorMiddleware);
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(
         `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
     );
+});
+
+// Handle Unhandled Promise rejection
+process.on('unhandledRejection', (err) => {
+    console.log(`ERROR: ${err}`);
+    console.log("Shutting down server due to Unhandled Promise Rejection");
+
+    server.close(() => {
+        process.exit(1);
+    })
 });
