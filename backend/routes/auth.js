26de09bfdb9756d9525fd106d9cdef8711b96b4c
KrishnaM023 <krishna050120>
2024-08-12 08:30:42 +0530

    Handle User Routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { forgotPassword, loginUser, logoutUser, registerUser, resetPassword } from '../controllers/authControllers.js';
-
+import { allUsers, deleteUser, forgotPassword, getUserDetails, getUserProfile, loginUser, logoutUser, registerUser, resetPassword, updatePassword, updateProfile, updateUser } from '../controllers/authControllers.js';
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js"
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -10,5 +10,18 @@ router.route("/logout").get(logoutUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allUsers);
+
+router
+  .route("/admin/users/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUser) 
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser); 
 
 export default router;
