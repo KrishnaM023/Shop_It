1470845cf6a345e2f7e6a92df8e111bb25f0238b
KrishnaM023 <krishna050120>
2024-08-14 09:43:40 +0530

    Implementing Redux toolKit & Handling Product display and details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/layout/footer";
 import Header from "./components/layout/header";
+import { Toaster } from "react-hot-toast";
+import ProductDetails from "./components/product/ProductDetails";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Toaster position="top-centre" />
         <Header />
 
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />}/>
+            <Route path="/product/:id" element={<ProductDetails />}/>
           </Routes>
         </div>
 
